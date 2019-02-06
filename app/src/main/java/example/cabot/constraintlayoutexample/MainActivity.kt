8979d564f34a9619abb49a3448a112af3e17325c
Kiran K J <kiran.jolly@cabotsolutions.com>
2019-02-06 14:27:22 +0530

    Initial commit

---
+++ app/src/main/java/example/cabot/constraintlayoutexample/MainActivity.kt
@@ -0,0 +1,14 @@
+package example.cabot.constraintlayoutexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+// reference
+// https://www.raywenderlich.com/9193-constraintlayout-tutorial-for-android-getting-started
